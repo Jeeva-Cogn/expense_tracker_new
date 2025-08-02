7daa007f4f5859983b03f390ecf72d7b8e304386
Jeeva-Cogn <jeeva.s2@cognizant.com>
2025-08-02 13:41:05 +0000

    Initial commit: Expense Tracker Flutter app with animation

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExpenseTrackerApp());
+}
+
+class ExpenseTrackerApp extends StatelessWidget {
+  const ExpenseTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ExpenseHomePage(),
+    );
+  }
+}
+
+
+class ExpenseHomePage extends StatefulWidget {
+  const ExpenseHomePage({super.key});
+
+  @override
+  State<ExpenseHomePage> createState() => _ExpenseHomePageState();
+}
+
+class _ExpenseHomePageState extends State<ExpenseHomePage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1200),
+      vsync: this,
+    );
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+      ),
+      body: Center(
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: const Text(
+            'Welcome to Expense Tracker!',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
